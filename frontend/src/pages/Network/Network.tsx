eb50f04758c264bd3054f66154756e614f0f11bc
Jamie Phillips <phillipsja97@gmail.com>
2025-02-27 23:54:09 -0600

    removed data folder

--- frontend/src/pages/Network/Network.tsx
+++ frontend/src/pages/Network/Network.tsx
@@ -1,22 +1,42 @@
-import { useEffect } from 'react'
-import { Button } from 'flowbite-react'
-import GetDevices from '../../data/tailscaleHttp/getDevices';
+import { useEffect, useState } from 'react'
+import { GetDevices } from '../../../bindings/github.com/phillipsja97/ProxUI-Wailsv3/tailscaleservice.ts';
+import { Device } from '../../../bindings/tailscale.com//client/tailscale/v2/models.ts';
 
 export default function Network() {
-    async function GetAllDevices () {
-        return await GetDevices();
-    }
+    const [devices, setDevices] = useState<Device[]>()
+
+    // async function GetAllDevices () {
+    //     return await GetDevices();
+    // }
 
     useEffect(() => {
-        const devices = GetAllDevices();
+        const fetchData = async () => {
+            try {
+                const response = await GetDevices();
+                setDevices(response);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
         console.log(devices);
-    })
+    },[GetDevices])
 
     return (
     <div id="networkPage" className="h-screen w-screen">
-        <div>
-            <Button gradientDuoTone="greenToBlue">I am the Network Page</Button>
-        </div>
+        {/* {
+            devices.map(device) => {
+               <Card className="max-w-sm">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Noteworthy technology acquisitions 2021
+                    </h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    </p>
+             </Card>
+            })
+        } */}
+        <h1>Hello</h1>
     </div>
     )
 }
